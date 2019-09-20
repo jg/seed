c5575dc4d4505b9aa3286d2c3bf598e602b0b6b9
Juliusz Gonera <juliusz.gonera@gmail.com>
2019-09-20 08:06:23 +0200

    pass the unrecognized commands through

--- dc.sh
+++ dc.sh
@@ -6,10 +6,10 @@ SERVICE=$2
 USER_COMMAND="${ARGS[@]:3}"
 case $1 in
   "r"|"run") CMD="run -u $UID:$GID $SERVICE $USER_COMMAND";;
-  "u"|"up") CMD="up $SERVICE";;
+  "u"|"up") CMD="up -d $SERVICE";;
   "b"|"build") CMD="build $SERVICE";;
   "c"|"console") CMD="run $SERVICE bash";;
-  *) echo "unknown command" && exit;;
+  *) CMD="${ARGS}"
 esac
 
 FULL="sudo docker-compose $CMD"
