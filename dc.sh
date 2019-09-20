d82bf8d9d7fb81f39a2cf34ef882e95d98c6ba30
Juliusz Gonera <juliusz.gonera@gmail.com>
2019-09-20 10:44:45 +0200

    add the user to sudoers

--- dc.sh
+++ dc.sh
@@ -5,9 +5,9 @@ ARGS=$@
 SERVICE=$2
 USER_COMMAND="${ARGS[@]:3}"
 case $1 in
-  "r"|"run") CMD="run -u $UID:$GID $SERVICE $USER_COMMAND";;
-  "u"|"up") CMD="up -d $SERVICE";;
-  "b"|"build") CMD="build $SERVICE";;
+  "r"|"run")     CMD="run -u $UID:$GID $SERVICE $USER_COMMAND";;
+  "u"|"up")      CMD="up -d $SERVICE";;
+  "b"|"build")   CMD="build $SERVICE";;
   "c"|"console") CMD="run $SERVICE bash";;
   *) CMD="${ARGS}"
 esac
